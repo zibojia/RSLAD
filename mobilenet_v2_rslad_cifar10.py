abec1ed4f24afb3b78dc087158156fb817840ba2
zibojia <38038800+zibojia@users.noreply.github.com>
2021-08-14 18:53:19 +0800

    Update mobilenet_v2_rslad_cifar10.py

--- mobilenet_v2_rslad_cifar10.py
+++ mobilenet_v2_rslad_cifar10.py
@@ -1,17 +1,19 @@
 import os
 import argparse
 import torch
-from trades_without_ce_loss import *
+from rslad_loss import *
 from cifar10_models import *
 import torchvision
 from torchvision import datasets, transforms
+import time
+# we fix the random seed to 0, in the same computer, this method can make the results same as before.
+torch.manual_seed(0)
+torch.cuda.manual_seed_all(0)
+torch.backends.cudnn.deterministic = True
 
-prefix = 'mobilenet_v2-teacher_wideresnet_RSLAD'
+prefix = 'resnet18-CIFAR10_RSLAD'
 epochs = 300
-lr = 0.1
-epsilon = 8/255.0
 batch_size = 128
-attack_iters = 10
 
 transform_train = transforms.Compose([
     transforms.RandomCrop(32, padding=4),
@@ -28,30 +30,31 @@ trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuff
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
 
-student = mobilenet_v2_ard()
+student = mobilenet_v2()
 student = torch.nn.DataParallel(student)
 student = student.cuda()
 student.train()
 optimizer = optim.SGD(student.parameters(), lr=0.1, momentum=0.9, weight_decay=2e-4)
 def kl_loss(a,b):
-    loss = -a*b
+    loss = -a*b + torch.log(b+1e-5)*b
     return loss
 teacher = wideresnet()
-teacher.load_state_dict(torch.load('./models/success_model/model_cifar_wrn.pt'))
+teacher.load_state_dict(torch.load('./models/model_cifar_wrn.pt'))
 teacher = torch.nn.DataParallel(teacher)
 teacher = teacher.cuda()
 teacher.eval()
 
-for epoch in range(235,epochs+1):
+for epoch in range(1,epochs+1):
     for step,(train_batch_data,train_batch_labels) in enumerate(trainloader):
+        start = time.time()
         student.train()
         train_batch_data = train_batch_data.float().cuda()
         train_batch_labels = train_batch_labels.cuda()
         optimizer.zero_grad()
         with torch.no_grad():
             teacher_logits = teacher(train_batch_data)
-        
-        adv_logits = trades_loss_without_celoss6(student,teacher_logits,train_batch_data,train_batch_labels,optimizer,step_size=2/255.0,epsilon=epsilon,perturb_steps=10)
+
+        adv_logits = rslad_inner_loss(student,teacher_logits,train_batch_data,train_batch_labels,optimizer,step_size=2/255.0,epsilon=epsilon,perturb_steps=10)
         student.train()
         nat_logits = student(train_batch_data)
         kl_Loss1 = kl_loss(F.log_softmax(adv_logits,dim=1),F.softmax(teacher_logits.detach(),dim=1))
@@ -61,6 +64,8 @@ for epoch in range(235,epochs+1):
         loss = 5/6.0*kl_Loss1 + 1/6.0*kl_Loss2
         loss.backward()
         optimizer.step()
+        end = time.time()
+        print(end-start)
         if step%100 == 0:
             print('loss',loss.item())
     if (epoch%20 == 0 and epoch <215) or (epoch%1 == 0 and epoch >= 215):
@@ -75,8 +80,7 @@ for epoch in range(235,epochs+1):
         test_accs = np.array(test_accs)
         test_acc = np.sum(test_accs==0)/len(test_accs)
         print('robust acc',np.sum(test_accs==0)/len(test_accs))
-        torch.save(student.state_dict(),'./models/'+prefix+str(np.sum(test_accs==0)/len(test_accs))+'.pth')
-        #torch.save({'student':student.state_dict(),'optimizer':optimizer.state_dict()},'./models/'+prefix+str(np.sum(test_accs==0)/len(test_accs))+'.pth')
+        torch.save({'student':student.state_dict(),'optimizer':optimizer.state_dict()},'./models/'+prefix+str(np.sum(test_accs==0)/len(test_accs))+'.pth')
     if epoch in [215,260,285]:
         for param_group in optimizer.param_groups:
             param_group['lr'] *= 0.1
